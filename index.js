a510f98dae3acf586a2a9d300adeafb76e5c0c4e
Danish Syed <cdoexploits@proton.me>
2024-04-11 20:38:26 -0500

    fixed moore bugs version 1.2.2

--- index.js
+++ index.js
@@ -55,7 +55,7 @@ app.get("/swru", async (req,res)=>{
 		let userid = query.userid;
 		let request = await axios.get(site);
 
-			axios.get(`https://api.thingspeak.com/update?api_key=${apikey}&field1=${"swru"+userid}-${encodeURIComponent(`${request.status}-${request.headers["content-type"]}-${request.headers["content-type"]=="application/json"?JSON.stringify(request.data):request.data}`)}`);
+			axios.get(`https://api.thingspeak.com/update?api_key=${apikey}&field1=${"swru"+userid}-${encodeURIComponent(`${request.status}-${request.headers["content-type"]}-${request.headers["content-type"].includes("application")?JSON.stringify(request.data):request.data}`)}`);
 			res.sendFile("1x1.gif",root);
 
 	} else {
